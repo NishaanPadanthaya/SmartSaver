610dd308744bc26d845c929f3b95ea42b9654f14
Mohammed Saqlain <saqlain2204@gmail.com>
2025-04-12 12:50:58 +0530

    Improved Securoity

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD12fR5yxzAL7SlLqBBtCXkmLNRY1TCcA8",
-  authDomain: "personal-finance-applica-af375.firebaseapp.com",
-  projectId: "personal-finance-applica-af375",
-  storageBucket: "personal-finance-applica-af375.firebasestorage.app",
-  messagingSenderId: "572400768725",
-  appId: "1:572400768725:web:13993240904eaee02e345c",
-  measurementId: "G-LBXGDX6X26"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
